505e9fc91f7e881f8b0f5675f0d29bb0d788435c
sean <sean.klein@outlook.de>
2022-06-10 17:24:47 +0200

    clean up

--- android/solutions/hands/src/main/java/com/google/mediapipe/examples/hands/PrefScreen.java
+++ android/solutions/hands/src/main/java/com/google/mediapipe/examples/hands/PrefScreen.java
@@ -1,21 +1,18 @@
 package com.google.mediapipe.examples.hands;
 
-import android.os.Build;
 import android.os.Bundle;
 
-import androidx.annotation.RequiresApi;
 import androidx.preference.MultiSelectListPreference;
-
 import androidx.preference.PreferenceFragmentCompat;
 
 public class PrefScreen extends PreferenceFragmentCompat {
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
         MultiSelectListPreference emojiListPref = (MultiSelectListPreference) findPreference("emoji_pref");
 
+        assert emojiListPref != null;
         emojiListPref.setEnabled(true);
 
     }
